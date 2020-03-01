25e3ec6569c22061ab983125b4990f3cd3ca64f2
Huseyin Civan <huseyincivan@gmail.com>
2020-03-01 17:58:57 -0500

    Google Seach Test

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -1,6 +1,7 @@
 package com.automation.tests.day2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
@@ -16,8 +17,17 @@ public class BasicNavigation {
 
         driver.get("http://www.google.com");
 
+        driver.findElement(By.xpath("//input[@title=\"Search\"]")).sendKeys("hello world");
+        driver.findElement(By.name("btnK")).click();
+
+        driver.navigate().refresh(); //to reload page
+        System.out.println("driver.getCurrentUrl() GOOGLE = " + driver.getCurrentUrl());
+
         driver.manage().window().maximize(); // to maximize browser
         driver.manage().window().fullscreen();
+        driver.manage().window().maximize();
+
+
 
 
         String title = driver.getTitle();
@@ -32,28 +42,43 @@ public class BasicNavigation {
         } else {
             System.out.println("TEST FAILED !");
         }
-
+        Thread.sleep(3000);  // for demo with 3 sec.
         driver.navigate().to("http://amazon.com");
 
-        if (driver.getTitle().toLowerCase().contains("amazon")) {
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone x 16gb");
+        driver.findElement(By.className("nav-input")).click();
+
+
+ //       if (driver.getTitle().toLowerCase().contains("amazon")) {
+        if(driver.getTitle().toLowerCase().contains("amazon")){
             System.out.println("Test Passed ! ");
         } else {
             System.out.println("Test failed !");
         }
         verifyEquals(driver.getTitle().toLowerCase(),"amazon");
-
+        Thread.sleep(3000);  // for demo with 3 sec.
         driver.navigate().back();
-
+        Thread.sleep(3000);  // for demo with 3 sec.
         verifyEquals(driver.getTitle(), "Google");
+        Thread.sleep(3000);  // for demo with 3 sec.
+        driver.navigate().forward();
+        verifyEquals(driver.getTitle().toLowerCase(),"amazon");
 
 
         Thread.sleep(3000);  // for demo with 3 sec.
 
+        //must be at the end !
         driver.close();
 
 
     }
 
+    //    /**
+//     * Check if to strings are same. If print TEST PASSED! message.
+//     * Otherwise, print TEST FAILED message
+//     * @param arg1
+//     * @param arg2
+//     */
     public static void verifyEquals(String arg1, String arg2) {
         if (arg1.equals(arg2)) {
             System.out.println("TEST PASSED"+" based on "+arg1+" vs "+" "+arg2);
@@ -64,3 +89,71 @@ public class BasicNavigation {
 
     }
 }
+//Vasyl
+//package com.automation.tests.day2;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.ie.InternetExplorerDriver;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.safari.SafariDriver;
+//public class BasicNavigation {
+//    public static void main(String[] args) throws Exception{
+//        //to start selenium script we need:
+//        //setup webdriver (browser driver) and create webdriver object
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+//        //In selenium, everything starts from WebDriver interface'
+//        //ChromeDriver extends RemoteWebDriver --> implements WebDriver
+//        driver.get("http://google.com");//to open a website
+//        driver.manage().window().maximize();//to maximize browser
+////        driver.manage().window().fullscreen();
+//        Thread.sleep(3000);//for demo, wait 3 seconds
+//        //method that return page title
+//        //you can also see it as tab name, in the browser
+//        String title = driver.getTitle();//returns <title>Some title</title> text
+//        String expectedTitle = "Google";//we provide it
+//        System.out.println("Title is..."+title);
+//        if(expectedTitle.equals(title)){
+//            System.out.println("TEST PASSED!");
+//        }else{
+//            System.out.println("TEST FAILED!");
+//        }
+//        //go to another website within the same window
+//        driver.navigate().to("http://amazon.com");
+//        Thread.sleep(3000);//for demo, wait 3 seconds
+//        if(driver.getTitle().toLowerCase().contains("amazon")){
+//            System.out.println("TEST PASSED!");
+//        }else{
+//            System.out.println("TEST FAILED!");
+//        }
+//        //comeback to google
+//        driver.navigate().back();
+//        Thread.sleep(3000);//for demo, wait 3 seconds
+//        //checking if page title is equals to Google
+//        //.getTitle() - returns page title
+//        verifyEquals(driver.getTitle(), "Google");
+//        //move forward in the browser history
+//        //again, going to amazon
+//        driver.navigate().forward();
+//        Thread.sleep(3000);//for demo, wait 3 seconds
+//        System.out.println("Title: "+driver.getTitle());
+//        //driver.getTitle() - returns page title of the page that is currently opened
+//        //must be at the end
+//        driver.close();//to close browser
+//        //browser cannot close itself
+//    }
+//    /**
+//     * Check if to strings are same. If print TEST PASSED! message.
+//     * Otherwise, print TEST FAILED message
+//     * @param arg1
+//     * @param arg2
+//     */
+//    public static void verifyEquals(String arg1, String arg2){
+//        if(arg1.equals(arg2)){
+//            System.out.println("TEST PASSED!");
+//        }else{
+//            System.out.println("TEST FAILED!");
+//        }
+//    }
---
+++ src/test/java/com/automation/tests/day2/GoogleSearchTest.java
@@ -0,0 +1,43 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        //By.name("q")  --> name="q"
+        // name - it's one of the selenium locaters
+        //there are 8 locators
+        //to choose locator, just use By.locator
+        WebElement search = driver.findElement(By.name("q"));
+        //once we found an element, how to enter text ?
+
+        //to enter text use sendKeys() method
+        //how to press Enter after entering the text?
+        //use Key.ENTER
+        //keys.ENTER - perform keyboard click
+        search.sendKeys("Java", Keys.ENTER);
+        Thread.sleep(2000);
+
+        //if see <a> element , it calls link
+        //visible text of this link , can be used by selenium to find this elemnt
+        WebElement news=driver.findElement(By.linkText("News"));
+        news.click(); //
+
+        Thread.sleep(2000);
+
+
+        driver.quit();
+    }
+}
